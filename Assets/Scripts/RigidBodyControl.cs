4d19eed6cb344c97a7663b66325515db7bcbd742
Unknown <sp3990@nyu.edu>
2017-02-16 15:11:18 -0500

    Init commit, working on rigidbody movement

---
+++ Assets/Scripts/RigidBodyControl.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RigidBodyControl : MonoBehaviour {
+
+	Rigidbody rb;
+
+	Vector3 inputVector;
+
+	bool isGrounded;
+
+	public float maxSpeed = 5f;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> (); //cache reference for our shortcut
+	}
+	
+	// get player input in Update
+	void Update () {
+		//MOVEMENT
+		float horizontal = Input.GetAxis ("Horizontal"); // Left/Right, A/D
+		float vertical = Input.GetAxis ("Vertical"); //Up/Down, W/S
+
+		inputVector = new Vector3 (horizontal, 0f, vertical);
+
+		//TURNING 
+		transform.Rotate(0f, Input.GetAxis("Mouse X") * Time.deltaTime * 180f, 0f);
+		//DO GROUNDED CHECK: shoot raycast just a little past bottom of capsule
+		isGrounded = Physics.Raycast(transform.position, Vector3.down, 1.1f);
+		//JUMPING
+		if (Input.GetKeyDown (KeyCode.Space) && isGrounded) {
+			inputVector.y = 60f;
+		}
+	}
+	// this is the framerate where physics runs
+	void FixedUpdate() {
+		//apply y movement force
+		rb.AddRelativeForce (Vector3.up * inputVector.y * 10f);
+		//apply x/z movement force
+		if(rb.velocity.magnitude < maxSpeed) {
+			rb.AddRelativeForce (inputVector * 10f);
+		}
+			
+
+	}
+}
---
+++ Assets/Scripts/physicsMovementScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class physicsMovementScript : MonoBehaviour {
+
+	Rigidbody rb;
+
+	Vector3 inputVector;
+
+	public float maxSpeed = 6f;
+
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> (); //caching reference
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//get steering input
+		float horizontal = Input.GetAxis ("Horizontal");
+		//acceleration and braking input
+		float vertical = Input.GetAxis("Vertical");
+
+		inputVector = new Vector3 (horizontal, 0f, vertical);
+
+		//steering controls based on input vector
+		transform.Rotate(0f, inputVector.x * Time.deltaTime * 180f, 0f);
+		
+	}
+
+	void FixedUpdate() {
+		
+		//apply acceleration if under maxspeed with vertical input axis
+		if (rb.velocity.magnitude < maxSpeed) {
+			rb.AddRelativeForce (0f, 0f, inputVector.z * Time.deltaTime);
+
+		}
+
+	}
+}
