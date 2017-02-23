9039e7c7766dca0f6c66b612974e2058ae815646
Unknown <sp3990@nyu.edu>
2017-02-22 19:03:44 -0500

    Road generation works!!!

--- Assets/Scripts/roadGenerate.cs
+++ Assets/Scripts/roadGenerate.cs
@@ -12,44 +12,53 @@ public class roadGenerate : MonoBehaviour {
 	bool goStraight;
 	bool isRight;
 	bool isLeft;
-	bool isStraight;
+	bool isStraight = true;
 	Vector3 lastRotation = new Vector3 (0, 0, 0);
 
 
 	// Use this for initialization
 	void Start () {
+		//cache lastRoad and create first road
 		lastRoad = Instantiate (roadPrefab);
+		//lastRoad.transform.position = new Vector3 (0, 0, 0);
 
 		for (int i = 0; i < roadLength; i++) {
+			//random number determines which turn to make
 			int genNum = Random.Range (0, 3);
 			//BoxCollider[] colliders = lastRoad.GetComponents<BoxCollider>()
 			GameObject nextRoad = Instantiate(roadPrefab);
+			//Straight
 			if (genNum == 1) {
 				goStraight = true;
 				turnLeft = false;
 				turnRight = false;
-				fixRotation();
+				fixRotation(nextRoad);
 				nextRoad.transform.parent = lastRoad.transform;
 				nextRoad.transform.localPosition = new Vector3 (-.0125f, 0, -1.235f);
-			} else if (genNum == 2) {
+
+			} //Left
+			else if (genNum == 2) {
 				goStraight = false;
 				turnLeft = true;
 				turnRight = false;
 				//nextRoad.transform.localEulerAngles = lastRotation + new Vector3 (0f, 270f, 0f);
-				fixRotation();
+				fixRotation(nextRoad);
 				nextRoad.transform.parent = lastRoad.transform;
 				nextRoad.transform.localPosition = new Vector3 (3.867048f, 0, -0.6110001f);
-			} else {
+
+			} //Right
+			else {
 				goStraight = false;
 				turnLeft = false;
 				turnRight = true;
 				//nextRoad.transform.localEulerAngles = lastRotation + new Vector3 (0f, 90f, 0f);
-				fixRotation();
+				fixRotation(nextRoad);
 				nextRoad.transform.parent = lastRoad.transform;
 				nextRoad.transform.localPosition = new Vector3 (-3.875433f, 0, -.6126667f);
 			}
 			lastRotation = nextRoad.transform.localEulerAngles;
 			lastRoad = nextRoad;
+			Debug.Log(i + ". " + isStraight + isRight + isLeft);
 		}
 	}
 		
@@ -59,31 +68,46 @@ public class roadGenerate : MonoBehaviour {
 
 	}
 
-	void fixRotation() {
+	void fixRotation(GameObject nextRoad) {
 		if (turnRight) {
 			if (isLeft) {
-
+				nextRoad.transform.localEulerAngles = new Vector3(0f, 0f, 0f);
+				isStraight = true;
+				isLeft = false;
 			} else if (isRight) {
-
+				nextRoad.transform.localEulerAngles = new Vector3(0f, 0f, 0f);
+				isStraight = true;
+				isRight = false;
 			} else if(isStraight) {
-
+				nextRoad.transform.localEulerAngles = new Vector3 (0f, 90f, 0f);
+				isStraight = false;
+				isRight = true; 
 			}
 		} else if (turnLeft) {
 			if (isLeft) {
-
+				nextRoad.transform.localEulerAngles = new Vector3(0f, 0f, 0f);
+				isStraight = true;
+				isLeft = false;
 			} else if (isRight) {
-
+				nextRoad.transform.localEulerAngles = new Vector3(0f, 0f, 0f);
+				isStraight = true;
+				isRight = false;
 			} else if (isStraight) {
-
+				nextRoad.transform.localEulerAngles = new Vector3 (0f, -90f, 0f);
+				isStraight = false;
+				isLeft = true;
 			}
 
 		} else if(goStraight) {
 			if (isLeft) {
-				
+				nextRoad.transform.localEulerAngles = new Vector3 (0f, -90f, 0f);
+				isLeft = true;
 			} else if (isRight) {
-
+				nextRoad.transform.localEulerAngles = new Vector3 (0f, 90f, 0f);
+				isRight = true;
 			} else if (isStraight) {
-
+				nextRoad.transform.localEulerAngles = new Vector3(0f, 0f, 0f);
+				isStraight = true;
 			}
 		}
 
